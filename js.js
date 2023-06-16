b39e88f30a5865c86a597cc235f4698de34df4ee
GuilhermeNairne <62121362+GuilhermeNairne@users.noreply.github.com>
2023-06-15 21:44:38 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+document.addEventListener("visibilitychange", function() {
+    if(document.hidden) {
+        document.title = "Volta aqui 😀"
+    } else {
+        document.title = "Portifolio"
+    }
+})
